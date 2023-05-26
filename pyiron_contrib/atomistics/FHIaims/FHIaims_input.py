efb82be947f36608ca081542a155dfe6516c6f1e
Joshua Schrier <jschrier@users.noreply.github.com>
2023-05-25 18:18:56 -0700

    input generation and job running works!  Now we need to implement the output parser...

--- pyiron_contrib/atomistics/FHIaims/FHIaims_input.py
+++ pyiron_contrib/atomistics/FHIaims/FHIaims_input.py
@@ -4,85 +4,64 @@ Input generation for FHI-aims
 
 # Imports
 import re
+import os
 from ase.atoms import Atoms as ASEAtoms, Atom as ASEAtom
 import ase.units as units
 
 
+
 # Functions
+
 def write_input(structure, 
-                xc ="pbe", 
-                relativistic = "atomic_zora scalar", 
-                relax_geometry = None,
-                relax_unit_cell = None,
-                spin = "none",
-                species = "light",
-                k_grid = None,
-                k_grid_density = None,
+                keyword_definitions,
                 control_filepath = "control.in",
                 geometry_filepath = "geometry.in",
-                species_filepath = "species_defaults/defaults_2020/"):
+                species_directory = "~/pyiron/resources/species_defaults/defaults_2020/light"):
     """write_input write input file for FHI-aims DFT code.
 
     Parameters
     ----------
     structure : ase.atoms.Atoms
         ASE atoms of structure you want to use.
-    xc : str, optional
-        dft xc functional to use, by default "pbe"
-    relativistic: str, optional
-        relativistic treatment to use, by default "atomic_zora scalar"
-    relax_unit_cell: Boolean, optional
-        should unit cell be relaxed in periodic systems, by default None
-    ...
-    # TODO:  write some more, once we decide on correct default choices
+    keyword_definitions : dictionary
+        Keys define the FHIaims keyword, values define settings
+    species : string (default = "light")
+        defines the type of species definition used 
+    control_filepath : string; default `control.in`
+    geometry_filepath : string; default `geometry.in`
+    species_filepath : string; default `~/pyiron/resources/FHIAims/species_defaults/defaults_2020/light`
+
     """
     # Variables
     control_filelines = []
     geometry_filelines = []
     unique_species_filenames = {}
 
-    control_filelines.append("xc {}\n".format(xc))
-    control_filelines.append("relativistic {}\n".format(relativistic))
-    control_filelines.append("spin {}\n".format(spin))
+    # define the first part of control.in
+    for key, value in keyword_definitions.items():
+        control_filelines.append("{}\t{}\n".format(key, value))
 
-    # TODO: smarter checking that supplied keywords are valid
-
-    if (relax_geometry != None):
-        control_filelines.append("relax_geometry {}\n".format(relax_geometry))
-
-    if (relax_unit_cell != None):
-        control_filelines.append("relax_unit_cell {}\n".format(relax_unit_cell))
-
-    # TODO: smarter checking between mutually exclusive options
-
-    if (k_grid != None):
-        control_filelines.append("k_grid {}\n".format(k_grid))
-
-    if (k_grid_density != None):
-        control_filelines.append("k_grid_density {}\n".format(k_grid_density))
-
-
-    # Find the elements and append the potentials
-    # TODO:  for now we only treat the defaults...
-    # TODO:  It's dodgy to assume unix-style filepaths joining... 
-
+    # Find the unique elements and identify the relevant filenames
     unique_species_filenames = set( 
-        ["{}/{}/{:02d}_{}_default".format(
-            species_filepath, species, Z, abbrev) 
+        ["{:02d}_{}_default".format(Z, abbrev) 
             for Z, abbrev 
             in zip(structure.get_atomic_numbers(),
                    structure.get_chemical_symbols())
         ]
     )
 
-    # write control.in file, along with the species info
+    #these should go into special subdirectories; make sure they exist
+    os.makedirs(os.path.dirname(control_filepath), 
+                exist_ok=True)
 
+    # write control.in file, along with the species info
     with open(control_filepath, 'w') as outFile:
         outFile.writelines(control_filelines)
         # append the species info.
         # safer to use python than to assume that cat exists
         for f in unique_species_filenames:
-            with open(f, 'r') as inFile:
+            species_file = os.path.join(species_directory, f)
+            with open(species_file, 'r') as inFile:
                 outFile.writelines(inFile.readlines())
         
 
@@ -92,6 +71,9 @@ def write_input(structure,
         "atom {} {} {} {}\n".format(pos[0], pos[1], pos[2], s)
         for s, pos in zip(structure.get_chemical_symbols(), structure.positions)
     ]    
+
+    os.makedirs(os.path.dirname(geometry_filepath), 
+                exist_ok=True)
     # Write geometry.in file
     with open(geometry_filepath, 'w') as outFile:
         outFile.writelines(geometry_filelines)
--- pyiron_contrib/atomistics/FHIaims/FHIaims_job.py
+++ pyiron_contrib/atomistics/FHIaims/FHIaims_job.py
@@ -1,7 +1,7 @@
 import os
 from pyiron_atomistics.atomistics.job.atomistic import AtomisticGenericJob
 from pyiron_atomistics.atomistics.structure.atoms import ase_to_pyiron
-from pyiron_base import DataContainer
+from pyiron_base import DataContainer, state
 from .FHIaims_input import write_input
 from .FHIaims_output import FHIaimsOutput
 
@@ -11,15 +11,13 @@ from .FHIaims_output import FHIaimsOutput
 # TODO: think more clearly about what behavior we actually want...I'm defaulting to molecular for now
 # TODO:  "species" was the best name that I could find for this, but I don't know...
 
+# setup some defaults
 input_dict = {
-    "xc": "pbe",
+    "xc": "pbe", 
     "relativistic": "atomic_zora scalar",
-    "relax_geometry": None,
-    "relax_unit_cell": None,
-    "spin": "none",  # "none" is defined string keyword, *not* python None
-    "species": "light",
-    "k_grid": None,
-    "k_grid_density": None
+    "relax_geometry": "bfgs 5e-3",
+    "spin": "none",
+    "species": "light"
 }
 
 class FHIaims(AtomisticGenericJob):
@@ -31,20 +29,42 @@ class FHIaims(AtomisticGenericJob):
         self._executable_activate()
         self._compress_by_default = True
 
+        project_config = state.settings
+        # TODO: for now we assume the first one is right; this is typically the user home version rather than conda...
+
+        # I'm getting an error that project doesn't have this
+        self.resource_directory = state.settings.resource_paths[0]
+
+        self.species_directory = os.path.join(self.resource_directory, 'FHIaims',  'species_defaults', 'defaults_2020')
+
+
+
+# TODO: Implement convenience functions and appropriate keywords
+    def calc_minimize(self, keywords):
+        print("TODO:  calc_minimize is not yet implemented; use calc_generic")
+
+    def calc_static(self, keywords):
+        print("TODO: calc_static is not yet implemented; use calc generic")
+
+    def calc_magnetic(self, keywords):
+        print("TODO: calc_magnetic is not yet implemented; use calc generic")
+
+    def calc_magnetic(self, keywords):
+        print("TODO: calc_magnetic is not yet implemented; use calc generic")
+
+
     def write_input(self):
+        keywords = self.input.to_builtin()
+        species = keywords["species"]
+        del keywords["species"]
+
         write_input(
             structure=self.structure,
-            xc=self.input["xc"],
-            relativistic=self.input["relativistic"],
-            relax_geometry=self.input["relax_geometry"],
-            relax_unit_cell=self.input["relax_unit_cell"],
-            spin=self.input["spin"],
-            species=self.input["species"],
-            k_grid=self.input["k_grid"],
-            k_grid_density=self.input["k_grid_density"],
+            keyword_definitions = keywords,
             control_filepath=os.path.join(self.working_directory, "control.in"),
-            geometry_filepath=os.path.join(self.working_directory, "geometry.in")
-        )
+            geometry_filepath=os.path.join(self.working_directory, "geometry.in"),
+            species_directory = os.path.join(self.species_directory, species))
+
 
 # TODO: implement output
 
